7f30fbedcf4ab50672e3dc0afdba28b1610a383c
sadia-afrin-mim <sadia.csedu@gmail.com>
2019-04-23 20:18:24 +0600

    Update posts.java

--- app/src/main/java/com/example/sadia/firebasetutorials/posts.java
+++ app/src/main/java/com/example/sadia/firebasetutorials/posts.java
@@ -6,9 +6,9 @@ package com.example.sadia.firebasetutorials;
 public class posts {
     String name;
 
-    String Genre;
-    String Catagory;
-    String Phone;
+    String genre;
+    String catagory;
+    String phone;
     String date;
 
     public posts() {
@@ -16,11 +16,11 @@ public class posts {
     }
 
 
-    public posts(String Name, String Genre, String Catagory, String Phone,String date) {
+    public posts(String Name, String genre, String catagory, String phone,String date) {
         this.name = Name;
-        this.Genre = Genre;
-        this.Catagory = Catagory;
-        this.Phone = Phone;
+        this.genre = genre;
+        this.catagory = catagory;
+        this.phone = phone;
         this.date = date;
     }
 
@@ -29,16 +29,16 @@ public class posts {
     }
 
 
-    public String getGenre() {
-        return Genre;
+    public String getgenre() {
+        return genre;
     }
 
-    public String getCatagory() {
-        return Catagory;
+    public String getcatagory() {
+        return catagory;
     }
 
-    public String getPhone() {
-        return Phone;
+    public String getphone() {
+        return phone;
     }
 
     public String getDate() {
