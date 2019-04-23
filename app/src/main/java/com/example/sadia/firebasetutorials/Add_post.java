c69f73e807d269966ddd99bc6b1f5e1232e4cd1a
sadia-afrin-mim <sadia.csedu@gmail.com>
2019-04-23 23:37:13 +0600

    Update Add_post.java

--- app/src/main/java/com/example/sadia/firebasetutorials/Add_post.java
+++ app/src/main/java/com/example/sadia/firebasetutorials/Add_post.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,7 +19,9 @@ public class Add_post extends AppCompatActivity {
     EditText Date_and_time;
     Spinner spinnerGenre;
     Button buttonAdd;
-    private DatabaseReference databaseUsers = FirebaseDatabase.getInstance().getReference("sadia");;
+    // private DatabaseReference databaseUsers = FirebaseDatabase.getInstance().getReference("sadia");;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseUsers = database.getReference("sadia");
 
 
 
@@ -41,7 +44,22 @@ public class Add_post extends AppCompatActivity {
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addArtist();
+                //addArtist();
+                String name = editTextName.getText().toString().trim();
+                String genre = spinnerGenre.getSelectedItem().toString();
+                String type = editTextType.getText().toString().trim();
+                String phone = editTextPhone.getText().toString().trim();
+                String d_t    = Date_and_time.getText().toString().trim();
+                if (!TextUtils.isEmpty(name)) {
+                    String id = databaseUsers.push().getKey();
+                    posts post = new posts(name, genre, type, phone,d_t);
+                    databaseUsers.child(id).setValue(post);
+
+
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_LONG); // initiate the Toast with context, message and duration for the Toast
+                    toast.show();
+                }
 
             }
         });
@@ -51,7 +69,7 @@ public class Add_post extends AppCompatActivity {
 
 
 
-    private void addArtist() {
+    /*private void addArtist() {
         String name = editTextName.getText().toString().trim();
         String genre = spinnerGenre.getSelectedItem().toString();
         String type = editTextType.getText().toString().trim();
@@ -64,9 +82,8 @@ public class Add_post extends AppCompatActivity {
 
 
         } else {
-
+            Toast toast = Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_LONG); // initiate the Toast with context, message and duration for the Toast
+            toast.show();
         }
-    }
+    }*/
 }
-
-
